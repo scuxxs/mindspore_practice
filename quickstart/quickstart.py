14adc0219b85997736d0b97bd00dbc3c498da7ef
scuxxs <93957347+scuxxs@users.noreply.github.com>
2022-08-30 17:01:45 +0800

    注释修改

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -1,25 +1,61 @@
 from mindvision.dataset import Mnist
 from mindvision.classification.models import lenet
 import mindspore.nn as nn
-from mindspore.train import Model
-from mindspore.train.callback import ModelCheckpoint, CheckpointConfig
+import mindspore as ms
 from mindvision.engine.callback import LossMonitor
+import numpy as np
+import matplotlib.pyplot as plt
 
-download_train = Mnist(path="./mnist", split="train", batch_size=32, shuffle=True, resize=32, download=True)
+# 下载并处理MNIST数据集
+download_train = Mnist(path="./mnist", split="train", batch_size=32, repeat_num=1, shuffle=True, resize=32,
+                       download=True)
+
 download_eval = Mnist(path="./mnist", split="test", batch_size=32, resize=32, download=True)
 
 dataset_train = download_train.run()
 dataset_eval = download_eval.run()
 
-network = lenet(num_classes=10)
+network = lenet(num_classes=10, pretrained=False)
+
+# 定义损失函数
 net_loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')
+
+# 定义优化器函数
 net_opt = nn.Momentum(network.trainable_params(), learning_rate=0.01, momentum=0.9)
 
-config_ck = CheckpointConfig(save_checkpoint_steps=1875, keep_checkpoint_max=10)
-ckpoint = ModelCheckpoint(prefix="lenet", directory="./lenet", config=config_ck)
+# 设置模型保存参数，模型训练保存参数的step为1875。
+config_ck = ms.CheckpointConfig(save_checkpoint_steps=1875, keep_checkpoint_max=10)
 
-model = Model(network, loss_fn=net_loss, optimizer=net_opt, metrics={'acc'})
-model.train(1, dataset_train, callbacks=[ckpoint, LossMonitor(0.01)])
+# 应用模型保存参数
+ckpoint = ms.ModelCheckpoint(prefix="lenet", directory="./lenet", config=config_ck)
 
-acc=model.eval(dataset_eval)
+# 初始化模型参数
+model = ms.Model(network, loss_fn=net_loss, optimizer=net_opt, metrics={'accuracy'})
+
+# 训练网络模型，并保存为lenet-1_1875.ckpt文件
+model.train(10, dataset_train, callbacks=[ckpoint, LossMonitor(0.01, 1875)])
+
+acc = model.eval(dataset_eval)
+
 print("{}".format(acc))
+
+
+mnist = Mnist("./mnist", split="train", batch_size=6, resize=32)
+dataset_infer = mnist.run()
+ds_test = dataset_infer.create_dict_iterator()
+data = next(ds_test)
+images = data["image"].asnumpy()
+labels = data["label"].asnumpy()
+
+plt.figure()
+for i in range(1, 7):
+    plt.subplot(2, 3, i)
+    plt.imshow(images[i-1][0], interpolation="None", cmap="gray")
+plt.show()
+
+# 使用函数model.predict预测image对应分类
+output = model.predict(ms.Tensor(data['image']))
+predicted = np.argmax(output.asnumpy(), axis=1)
+
+# 输出预测分类与实际分类
+print(f'Predicted: "{predicted}", Actual: "{labels}"')
